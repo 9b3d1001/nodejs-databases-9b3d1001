286221374aee260b8ad8b44abf65131938184b15
Ishaan Arora <ishaan.arora@nagarro.com>
2023-01-27 10:42:38 +0530

    Development Branch Push

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,82 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+const {MongoClient} = require ("mongodb");
 const CoinAPI = require('../CoinAPI');
 
 class MongoBackend {
 
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:27017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
+  }
 
-  async disconnect() {}
+  async disconnect() {
+    if(this.client) {
+      return this.client.close();
+    }
+    return false;
+  }
 
-  async insert() {}
+  async insert() {
+    const data = await this.coinAPI.fetch();
+    const documents = [];
+    Object.entries(data.bpi).forEach((entry) => {
+      documents.push({
+        date: entry[0],
+        value: entry[1]
+      });
+    });
+    return this.collection.insertMany(documents);
+  }
 
-  async getMax() {}
+  async getMax() {
+    return this.collection.findOne({}, {sort: {value: -1}});
+  }
 
-  async max() {}
+  async max() {
+    console.log("Connecting to MongoDB");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    if(client.isConnected()) {
+      console.log("Successfully connected to MongoDB");
+    } else {
+      throw new Error("MongoDB connection failed");
+    }
+    console.timeEnd("mongodb-connect");  
+    console.info("Inserting into MongoDB");
+    console.time("mongodb-insert");
+    const insertResult = await this.insert();
+    console.timeEnd("mongodb-insert");
+    console.info(`Inserted ${insertResult.result.n} documents in MongoDB`);
+    
+    console.info("Querying Mongo DB");
+    console.time("mongodb-find");
+    const document = await this.getMax();
+    console.timeEnd("mongodb-find");
+
+    console.log("Disconnecting from MongoDB");
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");  
+
+    return {
+      date: document.date,
+      value: document.value
+    };
+  }
 }
 
 module.exports = MongoBackend;
--- maxcoin/services/backend/RedisBackend.js
+++ maxcoin/services/backend/RedisBackend.js
@@ -1,32 +1,70 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+const Redis = require("ioredis");
 const CoinAPI = require('../CoinAPI');
 
 class RedisBackend {
 
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.client = null;
   }
 
-  async connect() {
-
+  connect() {
+    this.client = new Redis({
+      host: 'redis-19816.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+      port: 19816,
+      password: 'BjRk00vtqQzTKIBzNQjI8bOTpF7IWzlh'
+  });
+    return this.client;
   }
 
   async disconnect() {
-
+    this.client.disconnect();
   }
 
   async insert() {
-
+    const data = await this.coinAPI.fetch();
+    const values = [];
+    Object.entries(data.bpi).forEach((entries) => {
+      values.push(entries[1]);
+      values.push(entries[0]);
+    });
+    return this.client.zadd("maxcoin:values", values);
   }
 
   async getMax() {
-
+    return this.client.zrange("maxcoin:values", -1, -1, 'WITHSCORES');
   }
 
   async max() {
+    console.log("Connecting to Redis");
+    console.time("redis-connect");
+    const client = this.connect();
+    if(client) {
+      console.log("Successfully connected to Redis");
+    } else {
+      throw new Error("Redis connection failed");
+    }
+    console.timeEnd("redis-connect");  
+    console.info("Inserting into Redis");
+    console.time("redis-insert");
+    const insertResult = await this.insert();
+    console.timeEnd("redis-insert");
+    console.info(`Inserted ${insertResult} documents in Redis`);
+    
+    console.info("Querying Redis DB");
+    console.time("redis-find");
+    const result = await this.getMax();
+    console.timeEnd("redis-find");
 
+    console.log("Disconnecting from Redis");
+    console.time("redis-disconnect");
+    await this.disconnect();
+    console.timeEnd("redis-disconnect");  
+
+    return result;
   }
 }
 
